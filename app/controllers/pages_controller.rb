09024b9c5c28648d929aefdd08e88d7e2d65d360
thecookieorg <marko@thecookie.org>
2016-07-11 19:20:20 -0700

    added pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def portfolio
+  end
+
+  def products
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  get '/about' => 'pages#about'
+  get '/portfolio' => 'pages#portfolio'
+  get '/products' => 'pages#products'
+  get '/contact' => 'pages#contact'
+
+  root 'pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
